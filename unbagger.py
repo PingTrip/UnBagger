d5f890a5a4b26fd0349d4a98ec6ff93e99aeaa03
Dave Crawford <github@pingtrip.com>
2017-03-21 21:52:00 -0400

    Removed "noise filter". Enhanced coverage of obfuscation.

--- unbagger.py
+++ unbagger.py
@@ -5,21 +5,21 @@ from collections import Counter
 import sys
 import itertools
 
-__version__ = '1.1'
+__version__ = '1.5'
 __author__ = 'Dave Crawford'
 
 REGEXPS = {}
 
-REGEXPS['noiseFilter'] = r'^([a-z0-9]+)=.+\s*$'
+REGEXPS['varFinder'] = r'^([a-z0-9]+)="([a-z0-9]+)"\s*$'
 REGEXPS['deobFunc'] = r'([0-9a-z]+)\("[0-9a-z]+","?[0-9a-z]+"?\)'
 REGEXPS['grabFunc'] = r'function\s+FUNCNAME\(([a-z0-9]+),([a-z0-9]+)\)(.*?)end function'
-REGEXPS['grabStrings'] = r'FUNCNAME\("([a-z0-9]+)","([a-z0-9]+)"\)'
+REGEXPS['grabStrings'] = r'FUNCNAME\("([a-z0-9]+)"\s*,\s*(["a-z0-9]+)\)'
 REGEXPS['XORArray'] = r'Dim\s.*?([a-z0-9]+)\(\d\)'
 REGEXPS['XORArrayItem'] = r'ARRAYNAME\((\d)\)[^=,\n]'
 REGEXPS['XORKeyValue'] = r'ARRAYNAME\(ARRAYITEM\)\s*?=\s*?(\d+)'
 
 VBSSCRIPT = ""
-
+VARLIST = {}
 
 def loadVBS(fname):
     global VBSSCRIPT
@@ -30,41 +30,21 @@ def loadVBS(fname):
     VBSSCRIPT.replace(r"\r\n", r"\n")
 
     # Find all variable assingments (ex. Hqn0I="23")
-    rxp = re.compile(REGEXPS['noiseFilter'], re.I | re.M)
+    rxp = re.compile(REGEXPS['varFinder'], re.I | re.M)
     m = rxp.findall(VBSSCRIPT)
 
-    # Ensure variable name only appears once in script
-    delList = []
+    # Build table of variable assignments
+
     for var in m:
-        if VBSSCRIPT.count(var) == 1:
-            delList.append(var)
+        VARLIST[var[0]] = var[1]
 
-    # Delete noise from script
-    tmpVBSCRIPT = ""
-    for line in VBSSCRIPT.splitlines():
-        if not any(var in line for var in delList):
-            tmpVBSCRIPT += line + "\n"
-
-    VBSSCRIPT = tmpVBSCRIPT
-    return len(delList)
+    return
 
 
 def findDeobFunction():
     global VBSSCRIPT
     m = re.findall(REGEXPS['deobFunc'], VBSSCRIPT, re.I)
     return Counter(m).most_common(1)[0][0]
-
-
-def grabDeobFunc(funcName):
-    global VBSSCRIPT
-
-    tmp_grabFunc = REGEXPS['grabFunc'].replace('FUNCNAME', funcName)
-    m = re.search(tmp_grabFunc, VBSSCRIPT, re.I | re.DOTALL)
-    # [0] param1, [1] = param2, [2] = function code
-    print m.groups()[0]
-    print m.groups()[1]
-    print m.groups()[2]
-    return True
 
 
 def grabStrings(funcName):
@@ -75,6 +55,11 @@ def grabStrings(funcName):
 
 
 def deobString(arg1, arg2):
+    if '"' in arg2:
+        arg2 = arg2.replace('"','')
+    else:
+        arg2 = VARLIST[arg2.replace('"','')]
+
     retStr = ""
     s2h = []
     s2a = []
@@ -104,7 +89,7 @@ def findXORKey():
     m = re.search(REGEXPS['XORArray'], VBSSCRIPT, re.I | re.DOTALL)
     xorArray = m.groups(0)[0]
 
-    # find the single non-assignment reference to an array element
+    # find the single, non-assignment, reference to an array element
     m = re.search(REGEXPS['XORArrayItem'].replace('ARRAYNAME', xorArray), VBSSCRIPT, re.I | re.DOTALL)
     XORArrayItem = m.groups(0)[0]
 
@@ -120,6 +105,7 @@ def defang(url):
     li = m.groups(0)[0].rsplit('.', 1)
     li = '[.]'.join(li)
     url = url.replace(m.groups(0)[0], li)
+
     return url
 
 
@@ -127,9 +113,8 @@ def main(vbs):
     payloadURLS = []
 
     print "\n- Parsing VBS file..."
-    noise = loadVBS(vbs)
-    print "    + Removed {0} lines of noise...".format(noise)
-
+    loadVBS(vbs)
+    
     print "\n- Locating de-obfuscation function..."
     deobFunc = findDeobFunction()
 
@@ -138,6 +123,7 @@ def main(vbs):
 
     for m in grabStrings(deobFunc):
         ds = deobString(m[0], m[1])
+
         if "http" in ds:
             ds = defang(ds)
             payloadURLS.append(ds)
